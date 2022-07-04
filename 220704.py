0720d7f1ffc01a148d980e9e5b8bca28dd02faa3
BnBear123 <dlcodms792@gmail.com>
2022-07-04 21:16:03 +0900

    Update 220704.py

--- 220704.py
+++ 220704.py
@@ -2,3 +2,14 @@
 i = int(input())
 for k in range(1, i+1):
     print("*"*k)
+
+# 별찍기2
+i = int(input())
+for k in range(1, i+1):
+    print(" "*(i-k)+"*"*k)
+
+# 별찍기3
+i = int(input())
+for k in range(1, i+1):
+    print("*"*((i+1)-k))
+
